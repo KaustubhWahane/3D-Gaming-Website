4e6b716bb461ba9050a4c43deb864e50d955abaf
Lavda <kaustubhwahane55@gmail.com>
2024-07-04 19:58:12 +0530

    Adding Game Cards

--- 3DWebsite/src/App.jsx
+++ 3DWebsite/src/App.jsx
@@ -1,9 +1,11 @@
+import Categories from "./components/Categories/Categories";
 import GamingNavbar from "./components/GamingNavbar"; 
 
 export default function App() {
   return (
     <>
-      <GamingNavbar />
+      {/* <GamingNavbar/> */}
+      <Categories/>
     </>
   );
 }
