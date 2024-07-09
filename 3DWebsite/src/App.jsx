c7e4db703a9818e19ae27d2655361ab22f073161
Lavda <kaustubhwahane55@gmail.com>
2024-07-09 20:04:53 +0530

    Removed the Categories bar

--- 3DWebsite/src/App.jsx
+++ 3DWebsite/src/App.jsx
@@ -1,11 +1,9 @@
-import Categories from "./components/Categories/GameCategory";
 import GamingNavbar from "./components/GamingNavbar"; 
 
 export default function App() {
   return (
     <>
       <GamingNavbar/>
-      <Categories/>
     </>
   );
 }
